e53dcf225e29f3a9e2b908762e1919ff2fb59c31
varunch29 <varunreddynani29@gmail.com>
2025-07-23 11:42:29 +0530

    prescription

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -294,7 +294,7 @@ const DoctorAppointments = () => {
     <div className="w-full max-w-6xl m-5">
       <p className="mb-3 text-lg font-medium">All Appointments</p>
       <div className="bg-white border rounded text-sm max-h-[80vh] min-h-[50vh] overflow-y-scroll">
-        <div className="max-sm:hidden grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 py-3 px-6 border-b">
+        <div className="max-sm:hidden grid grid-cols-[0.5fr_1fr_1fr_0.5fr_1.5fr_1fr_1.5fr_1fr] gap-1 py-3 px-6 border-b">
           <p>#</p>
           <p>Patient</p>
           <p>Payment</p>
@@ -302,11 +302,12 @@ const DoctorAppointments = () => {
           <p>Date & Time</p>
           <p>Fee</p>
           <p>Action</p>
+          <p>Prescription</p>
         </div>
 
         {[...appointments].reverse().map((item, index) => (
           <div
-            className="flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50"
+            className="flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_1fr_1fr_0.5fr_1.5fr_1fr_1.5fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50"
             key={item._id}
           >
             <p className="max-sm:hidden">{index + 1}</p>

f0d1e517d5ddfe11ec6399190e3666646b109b5f
varunch29 <varunreddynani29@gmail.com>
2025-07-28 20:08:16 +0530

    prescription

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import MyAppointments from './pages/MyAppointments'
 import Login from './pages/Login'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
-import Contact from './pages/contact'
+import Contact from './pages/Contact'
 import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
